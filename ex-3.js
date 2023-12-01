582d63e204d7e25d805ee306e1be15ac5f09e33a
shnItt1 <144312080+shnItt1@users.noreply.github.com>
2023-12-01 14:06:46 +0700

    nam add hh-loop

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,9 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
 let minNumber;
-// Start coding here
+
+for (let i = 0; i < numbers.length; i++) {
+  minNumber = Math.min(i);
+}
 
 console.log(minNumber);
--- ex-6.js
+++ ex-6.js
@@ -2,6 +2,9 @@
 let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
-// Start coding here
+// เปลี่ยนค่าตั้งต้นให้ลบลำดับindexเพิ่มเริ่มจากหลังสุด กำหนดเงื่อนไขให้หยุดลูปเมื่อถึงindex0 โดยchangeจะลบลงเรื่อยๆเป้นการย้อนกลับ
+for (let i = companyName.length - 1; i >= 0; i--) {
+  reversedCompanyName = reversedCompanyName + companyName[i];
+}
 
 console.log(reversedCompanyName);
