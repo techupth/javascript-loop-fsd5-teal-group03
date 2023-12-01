83f78a9763f9bf7acebdf20cc6895b47afdfad4f
Nok <siriwan.skk@gmail.com>
2023-12-01 14:56:11 +0700

    feat: ex1-6 finish

--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,11 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
-
+//let text = "";
+let i = 0;
+while (i < restaurants.length) {
+    //text += "Restaurant Name: " + restaurants[i];
+    newRestaurants.push("Restaurant Name: " + restaurants[i]);
+    i++;
+}
 console.log(newRestaurants);
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,11 @@
 let companyName = "TechUp";
 
 // Start coding here
+let i=1;
+for (let letter of companyName) {
+    console.log("Number "+(i++)+" character is "+ letter);
+};
+
+// for (let i=0;i < companyName.length;i++) {
+//     console.log("Number "+(i+1)+" character is "+companyName[i]);
+// }
